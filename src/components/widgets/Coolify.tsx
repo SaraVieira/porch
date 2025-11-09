37c804030cdeb63af657e28230d461d3834004b3
SaraVieira <hey@iamsaravieira.com>
2025-11-09 18:53:37 +0000

    add coolify ui

--- src/components/widgets/Coolify.tsx
+++ src/components/widgets/Coolify.tsx
@@ -1,5 +1,23 @@
 import { useEffect, useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { CoolifyAPI } from '@/lib/types'
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemTitle,
+} from '../ui/item'
+import { Button } from '../ui/button'
+import { ArrowUpRight } from 'lucide-react'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
+import clsx from 'clsx'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '../ui/accordion'
 
 const getCoolifyData = async () => {
   const data = { services: [], applications: [] }
@@ -24,26 +42,108 @@ const getCoolifyData = async () => {
       },
     ).then((rsp) => rsp.json())
     data.applications = applications
-    return data
+    return data as CoolifyAPI
   } catch (error) {
     console.error('Error fetching Coolify data:', error)
   }
 }
 
 export const Coolify = () => {
-  const [coolifyData, setCoolifyData] = useState<{
-    services: Array<any>
-    applications: Array<any>
-  }>({ services: [], applications: [] })
+  const [coolifyData, setCoolifyData] = useState<CoolifyAPI>({
+    services: [],
+    applications: [],
+  })
   useEffect(() => {
     getCoolifyData().then(setCoolifyData)
   }, [])
+
+  const getStatusEl = (status: string) => {
+    const [a, b] = status.split(':')
+    const isHealthy = b.trim() === 'healthy'
+    const isRunning = a.trim() === 'running'
+
+    return (
+      <>
+        <div
+          className={clsx(
+            'rounded-full w-4 h-4',
+            isRunning ? 'bg-green-400' : 'bg-red-400',
+          )}
+        />
+        <div
+          className={clsx(
+            'rounded-full w-4 h-4',
+            isHealthy ? 'bg-green-400' : 'bg-yellow-400',
+          )}
+        />
+      </>
+    )
+  }
   return (
     <Card>
       <CardHeader>
         <CardTitle>Coolify</CardTitle>
       </CardHeader>
-      <CardContent>{JSON.stringify(coolifyData, null, 2)}</CardContent>
+      <CardContent className="flex flex-col gap-4">
+        <Accordion type="multiple" collapsible className="w-full">
+          <AccordionItem value="applications">
+            <AccordionTrigger>Applications</AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-4 text-balance">
+              {coolifyData.applications.map((app) => (
+                <Item variant="outline">
+                  <ItemContent>
+                    <ItemTitle>{app.name}</ItemTitle>
+                  </ItemContent>
+                  <ItemActions>
+                    <Tooltip>
+                      <TooltipTrigger className="flex gap-1">
+                        {getStatusEl(app.status)}
+                      </TooltipTrigger>
+                      <TooltipContent>{app.status}</TooltipContent>
+                    </Tooltip>
+                    <a
+                      href={app.fqdn}
+                      className="text-orange-accent"
+                      target="_blank"
+                    >
+                      <ArrowUpRight />
+                    </a>
+                  </ItemActions>
+                </Item>
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="services">
+            <AccordionTrigger>Services</AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-4 text-balance">
+              {coolifyData.services.map((app) => (
+                <Item variant="outline">
+                  <ItemContent>
+                    <ItemTitle className="flex items-center justify-between">
+                      {app.name}
+                    </ItemTitle>
+                  </ItemContent>
+                  <ItemActions>
+                    <Tooltip>
+                      <TooltipTrigger className="flex gap-1">
+                        {getStatusEl(app.status)}
+                      </TooltipTrigger>
+                      <TooltipContent>{app.status}</TooltipContent>
+                    </Tooltip>
+                    <a
+                      href={app.applications.find((a) => a.fqdn)?.fqdn || '#'}
+                      className="text-orange-accent"
+                      target="_blank"
+                    >
+                      <ArrowUpRight />
+                    </a>
+                  </ItemActions>
+                </Item>
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </CardContent>
     </Card>
   )
 }
